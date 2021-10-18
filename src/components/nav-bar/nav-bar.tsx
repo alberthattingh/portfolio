6f021ab3bf71aa701f7941a2b22c7ca471f32f30
Albert Hattingh <albert.h0504@gmail.com>
2021-10-18 21:46:08 +0200

    Responsive nav-bar

--- src/components/nav-bar/nav-bar.tsx
+++ src/components/nav-bar/nav-bar.tsx
@@ -1,9 +1,8 @@
-import { Image } from '@chakra-ui/image';
-import { Box, Center, Flex, SimpleGrid } from '@chakra-ui/layout';
+import { Center, Flex, SimpleGrid } from '@chakra-ui/layout';
 import { useColorModeValue } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
-import React from 'react';
-import logo from '../../logo.svg';
+import React, { useState } from 'react';
+import { HamburgerIcon } from '@chakra-ui/icons';
 
 export interface NavItem {
     name: string;
@@ -18,7 +17,13 @@ interface ButtonProps {
 
 function NavButton(props: ButtonProps): React.ReactElement {
     const { navItem, isSelected } = props;
-    const colour = useColorModeValue('gray.200', 'green.500');
+
+    const primaryColour = useColorModeValue('gray.200', 'green.500');
+    const colour = [
+        useColorModeValue('gray.200', 'white'),
+        useColorModeValue('gray.200', 'white'),
+        primaryColour,
+    ];
 
     return (
         <Flex
@@ -28,9 +33,13 @@ function NavButton(props: ButtonProps): React.ReactElement {
             color={isSelected ? 'white' : colour}>
             <Center
                 flex={1}
-                bg={isSelected ? colour : 'transparent'}
-                borderWidth={isSelected ? 1 : 0}
-                borderColor={colour}
+                bg={[
+                    'transparent',
+                    'transparent',
+                    isSelected ? primaryColour : 'transparent',
+                ]}
+                borderWidth={1}
+                borderColor={isSelected ? colour : 'transparent'}
                 _hover={{
                     textDecoration: 'none',
                     borderColor: colour,
@@ -59,18 +68,37 @@ interface Props {
 
 export default function NavBar(props: Props): React.ReactElement {
     const { navItems, selectedId } = props;
+    const [expanded, setExpanded] = useState(false);
 
     return (
         <Flex
             w='100%'
-            h='10vh'
-            bg={useColorModeValue('green.500', 'gray.800')}
+            h={[expanded ? '40vh' : '10vh', expanded ? '40vh' : '10vh', '10vh']}
+            bg={[
+                'green.500',
+                'green.500',
+                useColorModeValue('green.500', 'gray.800'),
+            ]}
             justifyContent='space-between'
             px={5}>
-            <Box w='25%' h='100%' bg=''>
-                <Image h='100%' minH='50px' src={logo} />
-            </Box>
-            <SimpleGrid w='50%' h='100%' columns={navItems.length} spacing={5}>
+            <Center w='10%' h='100%' bg=''>
+                <HamburgerIcon
+                    w={8}
+                    h={8}
+                    onClick={() => setExpanded(!expanded)}
+                    visibility={['visible', 'visible', 'hidden']}
+                />
+            </Center>
+            <SimpleGrid
+                visibility={[
+                    expanded ? 'visible' : 'hidden',
+                    expanded ? 'visible' : 'hidden',
+                    'visible',
+                ]}
+                w='50%'
+                h='100%'
+                columns={[1, 2, navItems.length]}
+                spacing={[0, 2, 5]}>
                 {navItems.map((item) => (
                     <NavButton
                         navItem={item}
